1ac13d4c3e8afdc70a62b61d18b156273c93483e
skchun <skchun@hawaii.edu>
2015-10-29 08:25:33 -1000

    Finished entry-1

---
+++ client/templates/posts/post_form.js
@@ -0,0 +1,17 @@
+Template.postForm.events({
+
+  // handle the form submission
+  'submit form': function(event, template) {
+
+    // stop the form from submitting
+    event.preventDefault();
+
+    Posts.insert({
+      title: event.target.title.value,
+      url: event.target.url.value
+    });
+
+    template.find("form").reset();
+  }
+
+});
